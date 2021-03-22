fbecca99d92de3e4bc7f6ddf73f4f1d9ff63e0cf
artemiy <artemiy.rozovyk@etu.upmc.fr>
2021-03-22 14:42:49 +0100

    Use generated html in the webview

--- src/ast_editor.ml
+++ src/ast_editor.ml
@@ -1,40 +1,8 @@
 open Import
 
-let _get_html_for_WebView_from_file =
+let get_html_for_WebView_from_file =
   let filename = Node.__dirname () ^ "/../react-app/dist/index.html" in
   In_channel.read_all filename
-
-let _get_html_for_WebView webview () =
-  let webviewUri =
-    WebView.asWebviewUri webview
-      ~localResource:(Path.asset "ast_view.js" |> Path.to_string |> Uri.file)
-    |> Uri.toString
-  in
-  " <!DOCTYPE html>\n\
-  \  <html lang=\"en\">\n\
-   <head>\n\
-  \        <title>PPM Preview</title>\n\
-  \        <meta charset=\" UTF-8\n\
-  \      \">\n\
-   <meta http-equiv=\"Content-Security-Policy\" content=\"default-src self; \
-   img-src vscode-resource:; script-src vscode-resource: 'self' \
-   'unsafe-inline'; style-src vscode-resource: 'self' 'unsafe-inline'; \"/>\n\n\
-  \          <style>\n\
-  \          #canvas {\n\
-  \            background-color: #347890;\n\
-  \            position: fixed;\n\
-  \            left: 50%;\n\
-  \            top: 50%;\n\
-  \            transform: translate(-50%, -50%);\n\
-  \          }\n\
-  \        </style>\n\
-  \      </head>\n\
-  \    <body>\n\n\
-  \   <textarea style=\" background-color:#347890; width: 700px; height: \
-   700px;\"></textarea>\n\n\
-  \      \n\
-  \      <script src=\"" ^ webviewUri ()
-  ^ "\" nonce=123 type =\"module\"></script>\n  </body>\n  </html>"
 
 (*^ (Path.asset "ast_view.js" |> Path.to_string) ^*)
 let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token :
@@ -47,7 +15,7 @@ let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token :
   WebView.set_options webview options;
   let listener _ = print_endline "i'm working!1" in
   let _ = WebView.onDidReceiveMessage webview ~listener () in
-  WebView.set_html webview _get_html_for_WebView_from_file;
+  WebView.set_html webview get_html_for_WebView_from_file;
 
   (*let disposable = Commands.registerCommand
     ~command:Extension_consts.Commands.open_to_the_side_ast_preview
@@ -62,8 +30,10 @@ let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token :
   let listener _event =
     let value = TextDocument.getText document () |> Dumpast.transform in
     let msg = Ojs.empty_obj () in
-    Ojs.set_prop_ascii msg "type" (Ojs.string_to_js "setValue");
-    Ojs.set_prop_ascii msg "value" (Ojs.string_to_js value);
+    Ojs.set msg "type" (Ojs.string_to_js "setValue");
+    (*Ojs.set msg "value" (Jsonoo.t_to_js _value);*)
+    (*let parseResult = Lift_test.to_js (TextDocument.getText document ()) in*)
+    Ojs.set msg "value" (Jsonoo.t_to_js value);
     let _ = WebView.postMessage webview msg in
     ()
   in
