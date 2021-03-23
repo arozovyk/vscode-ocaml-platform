eee52536ad74307f1ae0d2b6e21d9f7b6a0cc5d5
artemiy <artemiy.rozovyk@etu.upmc.fr>
2021-03-23 18:19:38 +0100

    Parse at launch and only on same document change

--- src/ast_editor.ml
+++ src/ast_editor.ml
@@ -1,20 +1,37 @@
 open Import
 
+let document_eq a b =
+  String.equal
+    (Uri.toString (TextDocument.uri a) ())
+    (Uri.toString (TextDocument.uri b) ())
+
 let get_html_for_WebView_from_file =
   let filename = Node.__dirname () ^ "/../react-app/dist/index.html" in
   In_channel.read_all filename
 
-let onDidReceiveMessageListener msg ~(document : TextDocument.t) =
+let transform_to_ast ~(document : TextDocument.t) ~(webview : WebView.t) =
+  let value = TextDocument.getText document () |> Dumpast.transform in
+  let msg = Ojs.empty_obj () in
+  Ojs.set msg "type" (Ojs.string_to_js "setValue");
+  Ojs.set msg "value" (Jsonoo.t_to_js value);
+  let _ = WebView.postMessage webview msg in
+  ()
+
+let onDidChangeTextDocument_listener event ~(document : TextDocument.t)
+    ~(webview : WebView.t) =
+  let changed_document = TextDocumentChangeEvent.document event in
+  if document_eq document changed_document then
+    transform_to_ast ~document ~webview
+  else
+    ()
+
+let onDidReceiveMessage_listener msg ~(document : TextDocument.t) =
   let cbegin = Int.of_string (Ojs.string_of_js (Ojs.get msg "begin")) in
   let cend = Int.of_string (Ojs.string_of_js (Ojs.get msg "end")) in
-  let eq a b =
-    String.equal
-      (Uri.toString (TextDocument.uri a) ())
-      (Uri.toString (TextDocument.uri b) ())
-  in
+
   let visibleTextEditors =
     List.filter (Vscode.Window.visibleTextEditors ()) ~f:(fun editor ->
-        eq (TextEditor.document editor) document)
+        document_eq (TextEditor.document editor) document)
   in
   let apply_selection editor =
     TextEditor.set_selection editor
@@ -35,7 +52,7 @@ let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token :
   WebView.set_options webview options;
   let _ =
     WebView.onDidReceiveMessage webview
-      ~listener:(onDidReceiveMessageListener ~document)
+      ~listener:(onDidReceiveMessage_listener ~document)
       ()
   in
   WebView.set_html webview get_html_for_WebView_from_file;
@@ -50,21 +67,13 @@ let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token :
     ~callback:(fun ~args:_ -> WebviewPanel.reveal webviewPanel ViewColumn.Two
     ~preserveFocus:false) in Vscode.ExtensionContext.subscribe extension
     ~disposable*)
-  let listener _event =
-    let value = TextDocument.getText document () |> Dumpast.transform in
-    let msg = Ojs.empty_obj () in
-    Ojs.set msg "type" (Ojs.string_to_js "setValue");
-    (*Ojs.set msg "value" (Jsonoo.t_to_js _value);*)
-    (*let parseResult = Lift_test.to_js (TextDocument.getText document ()) in*)
-    Ojs.set msg "value" (Jsonoo.t_to_js value);
-    let _ = WebView.postMessage webview msg in
-    ()
+  let _ =
+    Workspace.onDidChangeTextDocument
+      ~listener:(onDidChangeTextDocument_listener ~webview ~document)
+      ()
   in
-  let _ = Workspace.onDidChangeTextDocument ~listener () in
-  (* Vscode.ExtensionContext.subscribe extension ~disposable; *)
+  transform_to_ast ~document ~webview;
   CustomTextEditorProvider.ResolvedEditor.t_of_js (Ojs.variable "null")
-
-(*let listener event = let document = TextDocumentChangeEvent.document event in *)
 
 let register extension =
   let editorProvider =
